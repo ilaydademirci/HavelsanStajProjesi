01e696b42ed84d85bcbc8a3f46cfd52ced327137
Ilayda Demirci <ilaydademirci51@gmail.com>
2020-07-28 17:40:44 +0300

    TSI-3 için uyarlama yapıldı

--- StajProje/JiraHelper.cs
+++ StajProje/JiraHelper.cs
@@ -53,7 +53,7 @@ namespace StajProje
 
             var request = new RestRequest(Method.GET)
             {
-                Resource = "rest/api/3/issue/" + bugID + "/transitions"
+                Resource = "/rest/api/3/issue/TSI-2" + bugID + "/transitions"
             };
             request.AddHeader("Accept", "application/json");
             request.AddHeader("Content-Type", "application/json; charset=utf-8");
@@ -65,52 +65,53 @@ namespace StajProje
             return JsonConvert.DeserializeObject<JiraTransition>(response.Content);
         }
 
+        //TSI-3
         public void ControlJira()
         {
-            var client = new RestClient
+            var client2 = new RestClient
             {
                 BaseUrl = new Uri("https://temmuzhvlstaj.atlassian.net"),
                 Authenticator = new HttpBasicAuthenticator("ilaydademircii_@hotmail.com", "2sMXD5XlHRS2J0Q6tfLK46E8")
             };
 
-            var request = new RestRequest(Method.GET)
+            var request2 = new RestRequest(Method.GET)
             {
-                Resource = "rest/api/3/issue/TSI-3?expand=changelog"
+                Resource = "/rest/api/3/issue/TSI-3?expand=changelog"
             };
-            request.AddHeader("Accept", "application/json");
-            request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            request2.AddHeader("Accept", "application/json");
+            request2.AddHeader("Content-Type", "application/json; charset=utf-8");
 
-            request.RequestFormat = DataFormat.Json;
+            request2.RequestFormat = DataFormat.Json;
 
-            IRestResponse response = client.Execute(request);
+            IRestResponse response2 = client2.Execute(request2);
             
-            var jiraItem = JsonConvert.DeserializeObject<JiraItem>(response.Content);
+            var root = JsonConvert.DeserializeObject<Root>(response2.Content);
 
 
             //TSI-3 methodu transitionlarını alıyorum
-            var transitions = GetTransitions2("TSI-3");
+            var transitions2 = GetTransitions2("TSI-3");
 
         }
-        public JiraTransition GetTransitions2(string bugID)
+        public JiraTransition2 GetTransitions2(string bugID)
         {
-            var client = new RestClient
+            var client2 = new RestClient
             {
                 BaseUrl = new Uri("https://temmuzhvlstaj.atlassian.net"),
                 Authenticator = new HttpBasicAuthenticator("ilaydademircii_@hotmail.com", "2sMXD5XlHRS2J0Q6tfLK46E8")
             };
 
-            var request = new RestRequest(Method.GET)
+            var request2 = new RestRequest(Method.GET)
             {
-                Resource = "rest/api/3/issue/" + bugID + "/transitions"
+                Resource = "/rest/api/3/issue/TSI-3" + bugID + "/transitions"
             };
-            request.AddHeader("Accept", "application/json");
-            request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            request2.AddHeader("Accept", "application/json");
+            request2.AddHeader("Content-Type", "application/json; charset=utf-8");
 
-            request.RequestFormat = DataFormat.Json;
+            request2.RequestFormat = DataFormat.Json;
 
-            IRestResponse response = client.Execute(request);
+            IRestResponse response2 = client2.Execute(request2);
 
-            return JsonConvert.DeserializeObject<JiraTransition>(response.Content);
+            return JsonConvert.DeserializeObject<JiraTransition2>(response2.Content);
         }
     }
 }
